b475f2881414254efbe03de37893e7b0ca450be1
killermaxper01 <studyonlyjnv@gmail.com>
2025-12-28 12:34:46 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,30 +1,23 @@
 import { auth } from "./firebase.js";
 import {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  GoogleAuthProvider,
-  signInWithPopup
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-const provider = new GoogleAuthProvider();
+const path = window.location.pathname;
 
-window.login = async () => {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+onAuthStateChanged(auth, (user) => {
 
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-    window.location.href = "/";
-  } catch (e) {
-    alert(e.message);
+  // ❌ NOT LOGGED IN → BLOCK INDEX
+  if (!user && path.includes("index")) {
+    console.log("❌ Not logged in → redirect to login");
+    window.location.replace("/login.html");
+    return;
   }
-};
 
-window.googleLogin = async () => {
-  try {
-    await signInWithPopup(auth, provider);
-    window.location.href = "/";
-  } catch (e) {
-    alert(e.message);
+  // ✅ LOGGED IN → BLOCK LOGIN & SIGNUP
+  if (user && (path.includes("login") || path.includes("signup"))) {
+    console.log("✅ Logged in → redirect to index");
+    window.location.replace("/index.html");
   }
-};
+
+});
